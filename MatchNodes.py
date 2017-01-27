e7e088d01fd9c8da276e2ed86a25fb6d9a9ddccc
13t4074s <miko.ten.desires@gmail.com>
2017-01-27 16:21:22 +0900

    データの全経路探索をできるようにした

--- MatchNodes.py
+++ MatchNodes.py
@@ -1,7 +1,20 @@
+import time
 from py2neo import Graph
+from pandas import DataFrame
+
+start = time.time()
 
 graph = Graph("http://neo4j:mypassword@:7474/db/data/")
 
-dict = graph.data("MATCH (n) RETURN n")
+df = DataFrame(graph.data("""
+            MATCH (start: Person{name: 'A'}), (end: Person{name: 'D'}),
+            path = (start)-[*]->(end)
+            WITH EXTRACT(person IN NODES (path) | person.name) AS allpath
+            RETURN allpath;
+"""))
 
-print dict
+print df
+
+end = time.time() - start
+
+print ("\ntime:{0}".format(end) + "[sec]")
